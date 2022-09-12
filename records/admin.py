34ed0bbf1da60096823c3cef51290521c7eab954
Ashkan Shakiba <AshkanShakiba11@gmail.com>
2022-09-12 22:30:25 +0430

    fix some issues

--- records/admin.py
+++ records/admin.py
@@ -2,5 +2,25 @@ from django.contrib import admin
 
 from .models import IncreaseRecord, SaleRecord
 
-admin.site.register(IncreaseRecord)
-admin.site.register(SaleRecord)
+
+class IncreaseRecordAdmin(admin.ModelAdmin):
+    list_display = (
+        "seller",
+        "amount",
+        "date",
+        "completed",
+    )
+
+
+class SaleRecordAdmin(admin.ModelAdmin):
+    list_display = (
+        "seller",
+        "amount",
+        "phone_number",
+        "date",
+        "completed",
+    )
+
+
+admin.site.register(IncreaseRecord, IncreaseRecordAdmin)
+admin.site.register(SaleRecord, SaleRecordAdmin)
--- records/models.py
+++ records/models.py
@@ -8,15 +8,17 @@ class IncreaseRecord(models.Model):
     seller = models.ForeignKey(Seller, on_delete=models.CASCADE)
     date = models.DateTimeField(auto_now_add=True)
     completed = models.BooleanField(default=False)
+    checked = models.BooleanField(default=False)
 
     def __str__(self):
         return str(self.seller.name) + ":" + str(self.amount) + "(" + str(self.date) + ")"
 
     def save(self, force_insert=False, force_update=False, using=None, update_fields=None):
-        if not self.completed:
+        if not self.checked:
             self.seller.credit += self.amount
             self.seller.save()
             self.completed = True
+            self.checked = True
             super().save(force_insert=force_insert, force_update=force_update, using=using, update_fields=update_fields)
 
 
@@ -26,13 +28,14 @@ class SaleRecord(models.Model):
     phone_number = models.CharField(max_length=11, null=True)
     date = models.DateTimeField(auto_now_add=True)
     completed = models.BooleanField(default=False)
+    checked = models.BooleanField(default=False)
 
     def __str__(self):
         return str(self.seller.name) + "->" + str(self.phone_number) + ":" + str(self.amount) + \
                "(" + str(self.date) + ")"
 
     def save(self, force_insert=False, force_update=False, using=None, update_fields=None):
-        if not self.completed:
+        if not self.checked:
             credit = self.seller.credit - self.amount
             if credit >= 0:
                 self.seller.credit = credit
@@ -40,4 +43,5 @@ class SaleRecord(models.Model):
                 self.completed = True
             else:
                 pass
+            self.checked = True
             super().save(force_insert=force_insert, force_update=force_update, using=using, update_fields=update_fields)
